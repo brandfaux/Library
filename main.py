194128c647602cc15acec9d8f1cf5472ecf2b147
Brand Faux <brand.fauxx@gmail.com>
2024-02-26 19:33:12 +0530

    intial

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+app.run(debug=True)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')  
+def home():
+    return render_template('home.html') 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))  
+    return render_template('login.html', error=error)
+
+
+ 
