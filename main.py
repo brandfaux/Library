cc89b06c374d041ebdff7e7308d90ad2aa5a21c1
Brand Faux <brand.fauxx@gmail.com>
2024-03-01 12:50:46 +0530

    regis

--- main.py
+++ main.py
@@ -1,48 +1,86 @@
-from flask import Flask, request, redirect, url_for, session, render_template
-import mysql.connector
+from flask import Flask, redirect, url_for, render_template, request, session
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = 'hello'
 
-# MySQL Configuration
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Pass@123",
-    database="user_db"
-)
-cursor = db.cursor()
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = "Pass@123"
+app.config['MYSQL_DB'] = "user_db"
 
-# Function to authenticate user
-def authenticate(username, password):
-    query = "SELECT * FROM users WHERE username = %s AND password = %s"
-    cursor.execute(query, (username, password))
-    user = cursor.fetchone()
-    return user
+mysql = MySQL(app)
+
+
+def db_changes():
+    mysql.connection.commit()
+
 
 @app.route('/')
-def home():
-    if 'username' in session:
-        return f"Logged in as {session['username']}<br><a href='/logout'>Logout</a>"
-    return "You are not logged in<br><a href='/login'>Login</a>"
+def index():
+    return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST', 'GET'])
+# this is login
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = authenticate(username, password)
-        if user:
-            session['username'] = username
-            return redirect(url_for('home'))
-        else:
-            return "Invalid username or password"
+
+        # making connection
+        cur = mysql.connection.cursor()
+
+        check_if_in_db = cur.execute(
+            "SELECT * FROM users WHERE username = '{user}' AND password = '{passs}'".format(user=username,
+                                                                                            passs=password))
+
+        if check_if_in_db == 1:
+            session['user'] = username
+            session.permanent = True
+            return redirect('/inside')
+    else:
+        if 'user' in session:
+            return redirect(url_for('home_user'))
+
     return render_template('login.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        user = request.form['username']
+        passs = request.form['password']
+
+        if passs == passs:
+
+            cur1 = mysql.connection.cursor()
+            check_user_duplicate = cur1.execute("SELECT * FROM users WHERE username= '{name}'".format(name=user))
+            if check_user_duplicate > 0:
+                return 'User already exists'
+
+            else:
+                cur1.execute("INSERT INTO users (username, password) VALUES (%s,%s)", (user, passs))
+                db_changes()
+                return 'Register successfully'
+        else:
+            return 'Error not registered'
+    return render_template('register.html')
+
+
+@app.route('/inside')
+def home_user():
+    if 'user' in session:
+        user = session['user']
+        return f'Hello {user}'
+    else:
+        return redirect(url_for('login'))
+
 
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
-    return redirect(url_for('home'))
+    session.pop('user', None)
+    return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
